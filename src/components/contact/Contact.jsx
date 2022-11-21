df756e7badd2813e7c083e269f65a86d567efb55
Malith Ranasinghe <malith.ranasinghe@arimaclanka.com>
2022-11-22 00:20:02 +0530

    new changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,32 +1,32 @@
-import React, { useRef, useState } from "react";
-import emailjs from "@emailjs/browser";
+import React from "react";
+// import emailjs from "@emailjs/browser";
 import { MdOutlineEmail, MdOutlinePhone } from "react-icons/md";
 import "./contact.css";
 
 const Contact = () => {
-  const [message, setMessage] = useState(false);
-  const formRef = useRef();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessage(true);
-    emailjs
-      .sendForm(
-        "service_k2qawqh",
-        "template_c6rkpn6",
-        formRef.current,
-        "X7K7ebhIeOy3YwHki"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+  // const [message, setMessage] = useState(false);
+  // const formRef = useRef();
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setMessage(true);
+  //   emailjs
+  //     .sendForm(
+  //       "service_k2qawqh",
+  //       "template_c6rkpn6",
+  //       formRef.current,
+  //       "X7K7ebhIeOy3YwHki"
+  //     )
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
 
-    e.target.reset();
-  };
+  //   e.target.reset();
+  // };
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
