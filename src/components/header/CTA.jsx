84147b5c86071d1dde318076bcc00b26c2373d6e
Malith Ranasinghe <Malith.Ranasinghe@du.ae>
2023-06-11 15:21:45 +0530

    change

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-// import CV from '../../assets/Malith_Ranasinghe.pdf';
+import React from 'react';
+import CV from '../../assets/Malith_Ranasinghe.pdf';
 
 const CTA = () => {
-  return (
-    <div className="cta">
-      <a href={""} download className="btn">
-        Download CV
-      </a>
-      <a href="#contact" className="btn btn-primary">
-        Let's talk
-      </a>
-    </div>
-  );
+	return (
+		<div className="cta">
+			<a href={''} download className="btn">
+				Download CV
+			</a>
+			<a href="#contact" className="btn btn-primary">
+				Let's talk
+			</a>
+		</div>
+	);
 };
 
 export default CTA;
